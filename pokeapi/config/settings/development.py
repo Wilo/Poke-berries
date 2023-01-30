3f279be76610cfdb6ae55b57e8a711a80ad6cfe1
William Méndez <wmendezc19@gmail.com>
2023-01-30 12:54:38 -0500

    feat(api)!: modify project structure
    - Split settings into different stages.
    - Add support to Pytest.
    - Add support for Django Rest Framework Rest APIs.
    - Add support Swagger for API Documentation endpoints.

---
+++ pokeapi/config/settings/development.py
@@ -0,0 +1,28 @@
+from .base import *  # noqa
+from .base import env
+
+import secrets
+
+# GENERAL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#debug
+DEBUG = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default=str(secrets.token_hex(64)),
+)
+# https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+# django-debug-toolbar
+# ------------------------------------------------------------------------------
+# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
+INSTALLED_APPS += ["debug_toolbar"]  # noqa F405
+# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
+MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa F405
+# https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
+DEBUG_TOOLBAR_CONFIG = {
+    "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
+    "SHOW_TEMPLATE_CONTEXT": True,
+}
+# ------------------------------------------------------------------------------
